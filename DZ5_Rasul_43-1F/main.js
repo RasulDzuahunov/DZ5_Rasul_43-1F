e30c0d8bc8d40bba4ae81ca04c1bd174f1bb37ff
RasulDzuahunov <rasul.dju@gmail.com>
2024-10-24 17:14:14 +0600

    Add files via upload

---
+++ DZ5_Rasul_43-1F/main.js
@@ -0,0 +1,37 @@
+var input = document.getElementById("input");
+var createButton = document.getElementById("create_button");
+var todolist = document.getElementById("todo_list")
+
+var createTodo = () => {
+        if (!input.value.trim()) return alert ('Напиши что нибудь')
+    var div = document.createElement("div")
+    var text = document.createElement("h3")
+    var deleteBtn = document.createElement("button")
+    var editBtn = document.createElement("button")
+
+    deleteBtn.setAttribute("class", "delete_button")
+    editBtn.setAttribute("class", "edit_button")
+    div.setAttribute('class', 'block_text')
+
+    deleteBtn.innerHTML = "Delete"
+    editBtn.innerHTML = "Edit"
+    text.innerHTML = input.value;
+
+    deleteBtn.onclick = () => div.remove()
+
+    editBtn.onclick = () => {
+        var editedText = prompt(`Edit: ${text.innerHTML}`)
+        text.innerHTML = editedText
+    }
+
+    div.append(text, editBtn, deleteBtn);
+    todolist.append(div);
+
+    input.value = "";
+}
+
+createButton.addEventListener("click", createTodo);
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') createTodo()
+})
